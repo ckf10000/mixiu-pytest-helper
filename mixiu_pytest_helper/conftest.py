5460cce331726b46168ff777f5e750a2b14cfdcf
ckf10000 <ckf10000@sina.com>
2024-07-31 16:25:06 +0800

    first

--- mixiu_pytest_helper/conftest.py
+++ mixiu_pytest_helper/conftest.py
@@ -73,7 +73,7 @@ def redis_context() -> RedisClientManager:
 def run_tests(script_path: str = None):
     project_path = get_project_path()
     allure_dir = join_path([project_path, "allure-results"])
-    pytest_args = ['--alluredir={}'.format(allure_dir)]
+    pytest_args = ['--strict-markers', '--tb=short', '-v', '-ra', '-q', '-s', '--alluredir={}'.format(allure_dir)]
     if script_path is not None:
         if script_path == "__main__":
             script_path = sys.argv[0]
